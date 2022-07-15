e03faeff380ac1e4fba88d9f69bc76ee6e77eef2
Deivid S. Onorio <deividsonorio@gmail.com>
2022-07-14 22:00:54 -0300

    Updating Seeder

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Books;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class BooksSeeder extends Seeder
 {
@@ -17,5 +19,13 @@ class BooksSeeder extends Seeder
         Books::factory()
             ->count(10)
             ->create();
+
+        User::factory(
+            [
+                'name' => 'softdesign',
+                'email' => 'test@softdesign.com.br',
+                'password' => Hash::make('softdesignpass'),
+            ]
+        )->create();
     }
 }
